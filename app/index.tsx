4de200c77e2a5ef3942f5efc3fb0028d8e49c212
Gurneer <gurneer1223@gmail.com>
2025-07-11 12:34:02 -0400

    google sign in/sign out working

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,71 @@
-import { Text, View, Pressable, StyleSheet } from "react-native";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  isErrorWithCode,
+  isSuccessResponse,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
 import { useRouter } from "expo-router";
-import {GoogleSignin, GoogleSigninButton, statusCodes } from "@react-native-google-signin/google-signin"
+import { useEffect, useState } from "react";
+import { Button, Pressable, StyleSheet, Text, View } from "react-native";
 
 GoogleSignin.configure({
- 
-  iosClientId: '895289514692-lpihg3tl9unlc3thtklej03e6ckru1rl.apps.googleusercontent.com'
- 
+  iosClientId:
+    "895289514692-lpihg3tl9unlc3thtklej03e6ckru1rl.apps.googleusercontent.com",
 });
 export default function Index() {
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    const getCurrentUser = async () =>{
+      const response = await GoogleSignin.signInSilently();
+      if(response){
+       
+        setUser(response.data);
+      }
+    };
+    getCurrentUser();
+
+  }, []);
+  
+  console.log(user?.user.name);
+
+  // Somewhere in your code
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const response = await GoogleSignin.signIn();
+      if (isSuccessResponse(response)) {
+        setUser(response.data);
+      } else {
+        // sign in was cancelled by user
+      }
+    } catch (error) {
+      if (isErrorWithCode(error)) {
+        switch (error.code) {
+          case statusCodes.IN_PROGRESS:
+            // operation (eg. sign in) already in progress
+            break;
+          case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+            // Android only, play services not available or outdated
+            break;
+          default:
+          // some other error happened
+        }
+      } else {
+        // an error that's not related to google sign in occurred
+      }
+    }
+  };
+
+  const signOut = async () => {
+  try {
+    await GoogleSignin.signOut();
+    setUser( null ); // Remember to remove the user from your app's state as well
+  } catch (error) {
+    console.error(error);
+  }
+};
   const router = useRouter();
 
   return (
@@ -22,11 +80,20 @@ export default function Index() {
       >
         <Text style={styles.buttonText}>Go to Explore Page</Text>
       </Pressable>
-      <GoogleSigninButton/>
-    </View>
+      
+      {user ? (
+        <>
+          <Text style={{ fontSize: 26 }}>Hello {user.user.name}!</Text>
+          <Button title="Sign out" onPress={signOut} />
+        </>
+      ) : (
+        <GoogleSigninButton size={GoogleSigninButton.Size.Wide} color={GoogleSigninButton.Color.Dark} onPress={signIn} />
+      )}
+   </View>
   );
-}
+  }
 
+    
 const styles = StyleSheet.create({
   container: {
     flex: 1,
